805b323fda352918536dfd02425ff94108e5a50a
WangMingWei <1464228354@qq.com>
2020-07-10 22:22:28 +0800

    开始尝试

---
+++ storage/framework/views/fc598addc414af2e6accecd980762a6627bca83b.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>欢迎阁下观赏</title>
+    <link rel="stylesheet" href="/layui/css/layui.css">
+
+    <style>
+        .title {
+            text-align: center;
+            margin: 0 auto;
+            font-size: 60px;
+        }
+
+        .info {
+            margin-left:20px;
+            font-size: 20px;
+        }
+
+        .title2 {
+            text-align: center;
+            margin: 0 auto;
+            font-size: 50px;
+        }
+
+        .msg {
+            text-align: center;
+            font-size: 40px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+<h1 class="title">超级酷帅的 汪公子制作</h1>
+<hr class="layui-bg-red">
+<div class="layui-row">
+    <p calss="info">汪公子创办的公司</p>
+    <div calss="layui-col-xs10 layui-col-sm10 layui-col-md10">
+        <h2 class="title2">徐州平雷建材有限公司</h2>
+    </div>
+    <div calss="layui-col-xs10 layui-col-sm10 layui-col-md10">
+        <h2 class="title2">徐州鲲之大建材有限公司</h2>
+    </div>
+    <div class="layui-col-xs10 layui-col-sm10 layui-col-md10">
+        <h3 class="msg">永世长存!!</h3></div>
+</div>
+</body>
+<html><?php /**PATH /usr/share/nginx/html/web/cashfollow/resources/views/showHello.blade.php ENDPATH**/ ?>
